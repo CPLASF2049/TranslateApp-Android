ca0531df74305d6776fe4677dac9e74167a5c343
Ceriflor <ceriflor@163.com>
2024-05-13 19:15:00 +0800

    更改

--- app/src/main/java/com/example/translationapp/MainActivity.java
+++ app/src/main/java/com/example/translationapp/MainActivity.java
@@ -1,20 +1,147 @@
 package com.example.translationapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.ImageWriter;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
+import android.util.SparseArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextureView previewView;
+
+    HandlerThread handlerThread;
+    Handler cameraHandler;
+
+    CameraManager cameraManager;
+
+    Size previewSize;
+    Size captureSize;
+    String cameraID;
+
+    CameraDevice cameraDevice;
+
+    CaptureRequest captureRequest;
+    CaptureRequest.Builder captureRequestBuilder;
+
+    CameraCaptureSession cameraCaptureSession;
+
+    ImageReader imageReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        previewView = findViewById(R.id.textureView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startCameraThread();
+
+        if(!previewView.isActivated()){
+            previewView.setSurfaceTextureListener(textureListener);
+        }
+        else{
+            startPreview();
+        }
+    }
+
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+            try {
+                setUpCamera(width, height);
+            }
+            catch (CameraAccessException e){
+                throw new RuntimeException(e);
+            }
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {
+
+        }
+    }
+
+    private void startCameraThread() {
+
+        handlerThread = new HandlerThread("CameraThread");
+        handlerThread.start();
+        cameraHandler = new Handler(handlerThread.getLooper());
+
+    }
+
+    private void setUpCamera(int width, int height) throws cameraAccessException{
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        for(String CameraID : cameraManager.getCameraIdList()){
+            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraID);
+            Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+            if(facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT){
+                continue;
+            }
+
+        }
     }
 
 }
