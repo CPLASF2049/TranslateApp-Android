f882950ff73f2a7cd2909df106f642496b47eb8d
Ceriflor <ceriflor@163.com>
2024-06-06 17:12:09 +0800

    1

--- app/src/main/java/com/example/translationapp/TranslationActivity.java
+++ app/src/main/java/com/example/translationapp/TranslationActivity.java
@@ -14,9 +14,6 @@ public class TranslationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_translation);
-
-        translationResultTextView = findViewById(R.id.translation_result_text_view);
 
         TranslationManager translationManager = new TranslationManager();
         translationManager.translate("Hello, how are you", "en", "zh", "20240531002066446", "123456", "25a51c8844e4bc77929a307511480008", new TranslationManager.TranslationCallback() {
