a36d94b9065ccc4bd3a05aa7cbf32e8b0641d359
CPLASF2049 <318264563@qq.com>
2024-06-17 20:55:11 +0800

    修复了一些问题

--- app/src/main/java/com/example/translationapp/PrivacyPolicy.java
+++ app/src/main/java/com/example/translationapp/PrivacyPolicy.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,11 @@ public class PrivacyPolicy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.privacy_policy); // 确保这里的布局文件名是正确的
+
+
+        // 获取TextView并设置文本
+        TextView textViewPrivacyPolicy = findViewById(R.id.textViewPrivacyPolicy);
+        textViewPrivacyPolicy.setText("这是隐私政策的内容");
 
         // 初始化退出按钮并设置点击事件
         Button btnBack = findViewById(R.id.btn_back);
