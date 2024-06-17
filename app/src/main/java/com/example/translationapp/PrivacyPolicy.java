217e37f9ba8c6ec15706c40046cfddee67b77800
CPLASF2049 <318264563@qq.com>
2024-06-17 20:12:53 +0800

    修复了一些问题

--- app/src/main/java/com/example/translationapp/PrivacyPolicy.java
+++ app/src/main/java/com/example/translationapp/PrivacyPolicy.java
@@ -1,4 +1,32 @@
 package com.example.translationapp;
 
-public class PrivacyPolicy {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.privacy_policy); // 确保这里的布局文件名是正确的
+
+        // 初始化退出按钮并设置点击事件
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backToSettings(view);
+            }
+        });
+    }
+
+    // 按钮点击事件处理
+    public void backToSettings(View view) {
+        Intent intent = new Intent(PrivacyPolicy.this, SettingActivity.class);
+        startActivity(intent);
+    }
 }
